535491a33605a85fb3a3e334db2dfd0d6a8dafbc
Undernoone <coder729@foxmail.com>
2024-08-09 23:07:57 +0800

    0809

--- 1.Date_Operation.py
+++ 1.Date_Operation.py
@@ -4,48 +4,51 @@ import torch
 import numpy
 import os
 
-# x = torch.arange(12)
-# print(x)
-# print(x.shape)
-# print(x.numel())
-# x_reshape = x.reshape(3, 4)
-# print(x_reshape)
-# print(x_reshape.shape)
-#
-# all_zeros = torch.zeros((2,3,4)) # 二维三行四列的全零张量
-# print(all_zeros)
-# all_ones = torch.ones((2,3,4)) # 二维三行四列的全一张量
-# print(all_ones)
-# print('------------------------------------------------------------')
-#
-# a = torch.tensor([1,2,3],)
-# b = torch.tensor([4,5,6])
-# print(a+b)
-# print(torch.exp(a))
-# print('------------------------------------------------------------')
-#
-# c = torch.arange(12, dtype=torch.float32,).reshape(3,4)
-# d = torch.arange(12, 24, dtype=torch.float32,).reshape(3,4)
-# print(c)
-# print(d)
-# print(torch.cat([c,d],dim=0)) # dim的数值为维度，0表示按行拼接，1表示按列拼接
-# print(torch.cat([c,d],dim=1)) # dim的数值为维度，0表示按行拼接，1表示按列拼接
-# print('------------------------------------------------------------')
-#
-# demo = [[1,2,3],[4,5,6]]
-# demo_numpy = numpy.array(demo)
-# demo_tensor = torch.tensor(demo)
-# print(demo_numpy)
-# print(demo_tensor)
-# print(type(demo_numpy))
-# print(type(demo_tensor))
-# print('------------------------------------------------------------')
-#
-# e = torch.tensor([3.5])
-# print(e)
-# print(e.item())
-# print(type(e))
-# print(type(e.item())) # 转换为python的float类型
+x = torch.arange(12)
+print('x:',x)
+print('x.shape:',x.shape)
+print(x.numel()) # 返回int型的元素数量12
+x_reshape = x.reshape(3, 4)
+print(x_reshape)
+print(x_reshape.shape)
+print(x.numel()) # 返回int型的元素数量3*4=12
+print('------------------------------------------------------------')
+
+all_zeros = torch.zeros((2,3,4)) # 二维三行四列的全零张量
+print(all_zeros)
+all_ones = torch.ones((2,3,4)) # 二维三行四列的全一张量
+print(all_ones)
+print('------------------------------------------------------------')
+
+a = torch.tensor([1,2,3],)
+b = torch.tensor([4,5,6])
+print(a+b)
+print(torch.exp(a))
+print('------------------------------------------------------------')
+
+c = torch.arange(12, dtype=torch.float32,).reshape(3,4)
+d = torch.arange(12, 24, dtype=torch.float32,).reshape(3,4) # 左闭右开
+print(c)
+print(d)
+print(torch.cat([c,d],dim=0)) # dim的数值为维度，0表示按行拼接，1表示按列拼接
+print(torch.cat([c,d],dim=1)) # dim的数值为维度，0表示按行拼接，1表示按列拼接
+print('------------------------------------------------------------')
+
+demo = [[1,2,3],[4,5,6]]
+demo_numpy = numpy.array(demo)
+demo_tensor = torch.tensor(demo)
+print(demo_numpy)
+print(demo_tensor)
+print(type(demo))
+print(type(demo_numpy))
+print(type(demo_tensor))
+print('------------------------------------------------------------')
+
+e = torch.tensor([3.5])
+print(e)
+print(e.item())
+print(type(e))
+print(type(e.item())) # 转换为python的float类型
 
 os.makedirs(os.path.join("..", "Deeplearning_Li Mu_Date"), exist_ok=True) # 创建文件夹
 
---
+++ 24.Object_detection.py
@@ -0,0 +1,38 @@
+import os
+import pandas as pd
+import torch
+import torchvision
+from d2l import torch as d2l
+
+d2l.DATA_HUB['banana-detection'] = (
+    d2l.DATA_URL + 'banana-detection.zip',
+    '5de26c8fce5ccdea9f91267273464dc968d20d72')
+
+def read_data_bananas(is_train=True):
+    data_dir = d2l.download_extract('banana-detection')
+    csv_fname = os.path.join(data_dir, 'bananas_train' if is_train
+                             else 'bananas_val', 'label.csv')
+    csv_data = pd.read_csv(csv_fname)
+    csv_data = csv_data.set_index('img_name')
+    images, targets = [], []
+    for img_name, target in csv_data.iterrows():
+        images.append(torchvision.io.read_image(
+            os.path.join(data_dir, 'bananas_train' if is_train else
+                         'bananas_val', 'images', f'{img_name}')))
+        # Here `target` contains (class, upper-left x, upper-left y,
+        # lower-right x, lower-right y)
+        targets.append(list(map(float, target[['class', 'x1', 'y1', 'x2', 'y2']])) + [1])
+    return images, torch.tensor(targets).unsqueeze(1) / 256
+
+class BananasDataset(torch.utils.data.Dataset):
+    def __init__(self, is_train):
+        self.features, self.labels = read_data_bananas(is_train)
+        print('read ' + str(len(self.features)) + (
+            ' training examples' if is_train else ' validation examples'))
+
+    def __getitem__(self, idx):
+        return (self.features[idx].float(), self.labels[idx])
+
+    def __len__(self):
+        return len(self.features)
+
